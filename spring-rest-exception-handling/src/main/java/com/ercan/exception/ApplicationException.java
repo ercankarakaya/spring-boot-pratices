db08d2d7fbae7ef50ffba853e126507530901f3c
Ercan Karakaya <karakayaercan71@gmail.com>
2023-08-26 21:54:39 +0300

    Created ApplicationException class.

---
+++ spring-rest-exception-handling/src/main/java/com/ercan/exception/ApplicationException.java
@@ -0,0 +1,13 @@
+package com.ercan.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class ApplicationException extends RuntimeException{
+    private String errorCode;
+    private String message;
+    private HttpStatus httpStatus;
+}
