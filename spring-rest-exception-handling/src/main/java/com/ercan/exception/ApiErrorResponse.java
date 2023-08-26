65b354af583c7c00fb7861bf22a14322aa5acc12
Ercan Karakaya <karakayaercan71@gmail.com>
2023-08-26 23:01:41 +0300

    Created ApplicationExceptionHandler class.

--- spring-rest-exception-handling/src/main/java/com/ercan/exception/ApiErrorResponse.java
+++ spring-rest-exception-handling/src/main/java/com/ercan/exception/ApiErrorResponse.java
@@ -1,10 +1,12 @@
 package com.ercan.exception;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class ApiErrorResponse {
     private String guid;
     private String errorCode;
---
+++ spring-rest-exception-handling/src/main/java/com/ercan/exception/ApplicationExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.ercan.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Slf4j
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<?> handleApplicationException(final ApplicationException exception, final HttpServletRequest request) {
+        var guid = UUID.randomUUID().toString();
+        log.error(String.format("Error GUID=%s; Error Message: %s", guid, exception.getMessage()), exception);
+
+        var response = ApiErrorResponse
+                .builder()
+                .guid(guid)
+                .errorCode(exception.getErrorCode())
+                .message(exception.getMessage())
+                .statusCode(exception.getHttpStatus().value())
+                .statusName(exception.getHttpStatus().name())
+                .path(request.getRequestURI())
+                .method(request.getMethod())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(response, exception.getHttpStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleUnknownException(final Exception exception, final HttpServletRequest request) {
+        var guid = UUID.randomUUID().toString();
+        log.error(String.format("Error GUID=%s; Error Message: %s", guid, exception.getMessage()), exception);
+
+        var INTERNAL_SERVER_ERROR = HttpStatus.INTERNAL_SERVER_ERROR;
+        var response = ApiErrorResponse
+                .builder()
+                .guid(guid)
+                .errorCode(INTERNAL_SERVER_ERROR.name())
+                .message(INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .statusCode(INTERNAL_SERVER_ERROR.value())
+                .statusName(INTERNAL_SERVER_ERROR.name())
+                .path(request.getRequestURI())
+                .method(request.getMethod())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(response,INTERNAL_SERVER_ERROR);
+    }
+}
