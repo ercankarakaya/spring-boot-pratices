85d668aa5371880ce9d305ba84191e71f31a159f
Ercan Karakaya <karakayaercan71@gmail.com>
2023-08-27 04:27:26 +0300

    commit

--- spring-rest-exception-handling/src/main/java/com/ercan/model/Person.java
+++ spring-rest-exception-handling/src/main/java/com/ercan/model/Person.java
@@ -1,7 +1,6 @@
 package com.ercan.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -23,7 +23,7 @@ public class Person {
     private Long id;
     @NotNull(message = "Firstname can't be null")
     private String firstName;
-    @NotNull(message = "Lastname can't be null")
+    @NotBlank
     private String lastName;
     @Max(value = 100500,message = "Salary cannot be greater than 100500")
     private Double salary;
