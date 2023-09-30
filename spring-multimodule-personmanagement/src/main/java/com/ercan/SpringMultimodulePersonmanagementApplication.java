fe27e91198962642c6d1712e2460f3676ccbc74b
Ercan Karakaya <karakayaercan71@gmail.com>
2023-09-30 16:35:26 +0300

    commit

---
+++ spring-multimodule-personmanagement/src/main/java/com/ercan/SpringMultimodulePersonmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.ercan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultimodulePersonmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultimodulePersonmanagementApplication.class, args);
+    }
+
+}
