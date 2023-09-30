8d0597867431cdb3722e4d726f02c36da81d15c5
Ercan Karakaya <karakayaercan71@gmail.com>
2023-09-30 18:53:13 +0300

    Person model class created.

---
+++ spring-multimodule-personmanagement/domain/src/main/java/com/ercan/model/Person.java
@@ -0,0 +1,24 @@
+package com.ercan.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Double salary;
+}
