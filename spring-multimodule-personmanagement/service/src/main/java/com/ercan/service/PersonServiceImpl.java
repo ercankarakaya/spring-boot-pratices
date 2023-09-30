8e4fb0eab7a8fe1a84c7b75c318e41b0c19651da
Ercan Karakaya <karakayaercan71@gmail.com>
2023-09-30 19:35:25 +0300

    Person service methods logging

--- spring-multimodule-personmanagement/service/src/main/java/com/ercan/service/PersonServiceImpl.java
+++ spring-multimodule-personmanagement/service/src/main/java/com/ercan/service/PersonServiceImpl.java
@@ -2,6 +2,8 @@ package com.ercan.service;
 
 import com.ercan.model.Person;
 import com.ercan.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +15,16 @@ import java.util.stream.Stream;
 @Service
 public class PersonServiceImpl implements PersonService{
 
+    private static final Logger logger = LoggerFactory.getLogger(PersonService.class);
+
     @Autowired
     private PersonRepository personRepository;
 
 
     @PostConstruct
     public void init(){
+        logger.info("PersonServiceImpl.init() method called.");
+
         List<Person> personList = Stream.of
                 (
                 new Person("Micheal","Jackson"),
@@ -31,6 +37,7 @@ public class PersonServiceImpl implements PersonService{
 
     @Override
     public List<Person> getPersonAll(){
+        logger.info("PersonServiceImpl.getPersonAll() method called.");
         return personRepository.findAll();
     }
 }
